79d1e6fabd2181d3ec272f2fb0061a9d48e6d33a
Nikita <nikich340@gmail.com>
2016-02-12 08:15:21 +1000

    Drop full_<device>.mk
    Change-Id: I0f5154a93752d554d768c2be2d7428a1465b5cd6

--- cm.mk
+++ cm.mk
@@ -1,5 +1,7 @@
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, device/lge/g2m/device_g2m.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 540
@@ -8,7 +10,7 @@ TARGET_SCREEN_HEIGHT := 960
 # Release name
 PRODUCT_RELEASE_NAME := LG G2 Mini
 PRODUCT_NAME := cm_g2m
-PRODUCT_MANUFACTURER := lge
 PRODUCT_MODEL := g2m
-
-$(call inherit-product, device/lge/g2m/full_g2m.mk)
+PRODUCT_DEVICE := g2m
+PRODUCT_MANUFACTURER := LGE
+PRODUCT_BRAND := lge
