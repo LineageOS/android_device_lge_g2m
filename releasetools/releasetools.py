f70cf32544708beecb2d26f48436854652366841
MobiusM <danholandi@gmail.com>
2017-01-25 00:32:07 +0200

    g2m: re-do releasetool.py
    Change-Id: I9671f9ef9e6aae53d276c3da5e506063f882ba4d

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -13,19 +13,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
+import common
+import re
 
-TARGET_DIR = os.getenv('OUT')
-def InstallEnd_SetSpecificDeviceConfigs(self):
-  self.output_zip.write(os.path.join(TARGET_DIR, "fixup.sh"), "fixup.sh")
-  self.script.AppendExtra('package_extract_file("fixup.sh", "/tmp/fixup.sh");')
-  self.script.AppendExtra('set_metadata("/tmp/fixup.sh", "uid", 0, "gid", 0, "mode", 0755);')
-  self.script.Mount("/system")
-  self.script.AppendExtra('run_program("/tmp/fixup.sh");')
+def InstallEnd_SetSpecificDeviceConfigs(info):
+    info.script.Mount("/system");
+    info.script.AppendExtra('run_program("/tmp/install/bin/fixup.sh")');
+    info.script.Unmount("/system");
+    return
 
-def FullOTA_InstallEnd(self):
-  InstallEnd_SetSpecificDeviceConfigs(self)
+def FullOTA_InstallEnd(info):
+  InstallEnd_SetSpecificDeviceConfigs(info)
 
-def IncrementalOTA_InstallEnd(self):
-  InstallEnd_SetSpecificDeviceConfigs(self)
+
+def IncrementalOTA_InstallEnd(info):
+  InstallEnd_SetSpecificDeviceConfigs(info)
 
